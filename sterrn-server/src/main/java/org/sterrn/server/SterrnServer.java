e4cb6c2c79a19138d272fd4033f5a81a3054571d
Luis Weich <luis.weich@study.thws.de>
2024-04-09 18:24:27 +0200

    add SterrnServer version logging

--- sterrn-server/src/main/java/org/sterrn/server/SterrnServer.java
+++ sterrn-server/src/main/java/org/sterrn/server/SterrnServer.java
@@ -3,9 +3,14 @@ package org.sterrn.server;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sterrn.core.Sterrn;
 import org.sterrn.core.util.Lazy;
 
 public class SterrnServer extends Application<SterrnServerConfiguration> {
+
+    private static final Logger log = LoggerFactory.getLogger(SterrnServer.class);
 
     private static final Lazy<SterrnServer> instance = Lazy.of(SterrnServer::new);
 
@@ -19,6 +24,7 @@ public class SterrnServer extends Application<SterrnServerConfiguration> {
 
     @Override
     public void run(SterrnServerConfiguration configuration, Environment environment) {
+        log.info("Run SterrnServer - Version {}", Sterrn.VERSION);
     }
 
 }
