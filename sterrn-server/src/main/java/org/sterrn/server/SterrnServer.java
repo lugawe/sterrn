55e8f7824af9fe09b0f4d5cb561f60838a061bb2
Luis Weich <luis.weich@study.thws.de>
2024-04-09 17:56:57 +0200

    main SterrnServer

--- sterrn-server/src/main/java/org/sterrn/server/SterrnServer.java
+++ sterrn-server/src/main/java/org/sterrn/server/SterrnServer.java
@@ -1,4 +1,24 @@
 package org.sterrn.server;
 
-public class SterrnServer {
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Bootstrap;
+import io.dropwizard.core.setup.Environment;
+import org.sterrn.core.util.Lazy;
+
+public class SterrnServer extends Application<SterrnServerConfiguration> {
+
+    private static final Lazy<SterrnServer> instance = Lazy.of(SterrnServer::new);
+
+    public static void main(String[] args) throws Exception {
+        instance.get().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<SterrnServerConfiguration> bootstrap) {
+    }
+
+    @Override
+    public void run(SterrnServerConfiguration configuration, Environment environment) {
+    }
+
 }
---
+++ sterrn-server/src/main/java/org/sterrn/server/SterrnServerConfiguration.java
@@ -0,0 +1,6 @@
+package org.sterrn.server;
+
+import io.dropwizard.core.Configuration;
+
+public class SterrnServerConfiguration extends Configuration {
+}
