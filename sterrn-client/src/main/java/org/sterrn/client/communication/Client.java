dae8cf6a31e2afb1a6e147b9c4a71e82f5bb1d78
Luis Weich <luis.weich@study.thws.de>
2024-04-09 19:24:06 +0200

    updates

---
+++ sterrn-client/src/main/java/org/sterrn/client/communication/Client.java
@@ -0,0 +1,13 @@
+package org.sterrn.client.communication;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class Client {
+
+    @Inject
+    public Client() {
+    }
+
+}
---
+++ sterrn-server/src/main/java/org/sterrn/server/communication/Server.java
@@ -0,0 +1,13 @@
+package org.sterrn.server.communication;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class Server {
+
+    @Inject
+    public Server() {
+    }
+
+}
