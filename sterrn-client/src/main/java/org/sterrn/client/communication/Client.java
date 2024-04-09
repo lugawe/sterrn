66354e37eb0199a98b12800c75ca42e0d8317454
Luis Weich <luis.weich@study.thws.de>
2024-04-09 19:31:17 +0200

    updates

--- sterrn-client/src/main/java/org/sterrn/client/communication/Client.java
+++ sterrn-client/src/main/java/org/sterrn/client/communication/Client.java
@@ -2,12 +2,23 @@ package org.sterrn.client.communication;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.sterrn.core.communication.Sender;
+import org.sterrn.core.util.Lazy;
+
+import java.util.UUID;
 
 @Singleton
-public class Client {
+public class Client implements Sender {
+
+    private final Lazy<UUID> id = Lazy.of(UUID::randomUUID);
 
     @Inject
     public Client() {
+    }
+
+    @Override
+    public UUID getId() {
+        return id.get();
     }
 
 }
--- sterrn-server/src/main/java/org/sterrn/server/communication/Server.java
+++ sterrn-server/src/main/java/org/sterrn/server/communication/Server.java
@@ -2,12 +2,23 @@ package org.sterrn.server.communication;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import org.sterrn.core.communication.Sender;
+import org.sterrn.core.util.Lazy;
+
+import java.util.UUID;
 
 @Singleton
-public class Server {
+public class Server implements Sender {
+
+    private final Lazy<UUID> id = Lazy.of(UUID::randomUUID);
 
     @Inject
     public Server() {
+    }
+
+    @Override
+    public UUID getId() {
+        return id.get();
     }
 
 }
