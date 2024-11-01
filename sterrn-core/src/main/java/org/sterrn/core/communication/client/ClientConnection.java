1f546b1184c134c249793433409b3e76dbeac886
Lugawe <luis.weich.de@gmail.com>
2024-11-01 16:33:04 +0100

    ClientConnection add sendMessage method

--- sterrn-core/src/main/java/org/sterrn/core/communication/client/ClientConnection.java
+++ sterrn-core/src/main/java/org/sterrn/core/communication/client/ClientConnection.java
@@ -1,9 +1,16 @@
 package org.sterrn.core.communication.client;
 
+import org.sterrn.core.communication.Message;
+import org.sterrn.core.util.Result;
+
 public interface ClientConnection {
 
     void connect() throws Exception;
 
     void disconnect() throws Exception;
+
+    Result<Message> sendMessage(Message message, long timeout);
+
+    Result<Message> sendMessage(Message message);
 
 }
